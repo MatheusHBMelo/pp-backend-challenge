e602136ab1fcedf1a046c1237c5f7431e4c506d1
MatheusHBMelo <matheushbmelo@gmail.com>
2024-06-26 10:17:52 -0300

    Feat: Adds validation to fields mapped by hibernate.

--- src/main/java/tech/mhbm/pp_backend/domain/controllers/dtos/TransferRequest.java
+++ src/main/java/tech/mhbm/pp_backend/domain/controllers/dtos/TransferRequest.java
@@ -1,7 +1,12 @@
 package tech.mhbm.pp_backend.domain.controllers.dtos;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
-public record TransferRequest(Long payerId, Long payeeId, BigDecimal value) {
+public record TransferRequest(@NotNull(message = "PayerId cannot be null") Long payerId,
+                              @NotNull(message = "PayeeId cannot be null") Long payeeId,
+                              @NotNull(message = "Value cannot be null") @DecimalMin(value = "0.01", message = "value cannot be less than $0.01") BigDecimal value) {
 
 }
--- src/main/java/tech/mhbm/pp_backend/domain/controllers/dtos/WalletRequestDto.java
+++ src/main/java/tech/mhbm/pp_backend/domain/controllers/dtos/WalletRequestDto.java
@@ -1,13 +1,15 @@
 package tech.mhbm.pp_backend.domain.controllers.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import tech.mhbm.pp_backend.domain.models.Wallet;
 import tech.mhbm.pp_backend.domain.models.WalletType;
 
-public record WalletRequestDto(String name,
-                               String cpfCnpj,
-                               String email,
-                               String password,
-                               WalletType.Enum walletType) {
+public record WalletRequestDto(@NotBlank(message = "Name cannot be blank") String name,
+                               @NotBlank(message = "CpfCnpj cannot be blank") String cpfCnpj,
+                               @NotBlank(message = "Email cannot be blank") String email,
+                               @NotBlank(message = "Password cannot be blank") String password,
+                               @NotNull(message = "WalletType cannot be null") WalletType.Enum walletType) {
     public Wallet toWallet() {
         return new Wallet(name, cpfCnpj, email, password, walletType.get());
     }
